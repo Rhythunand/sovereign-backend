3802e21b7229c81e148c53dae11027b771daa1de
Rhythunand <rhythunandbiju@gmail.com>
2026-01-04 21:26:44 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 // This URL must match your Node.js server port
-const API_URL = 'https://sovereign-backend-6pcz.onrender.com';
+const API_URL = 'https://sovereign-backend-gj6y.onrender.com';
 
 /* =======================
     USER SIGNUP
@@ -90,4 +90,5 @@ async function loginUser() {
     }
 
 }
+
 
